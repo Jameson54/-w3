a5c81424db43688f5e49afc782565c67f6a99fa5
Jameson54 <hehlol123123@mail.ru>
2025-09-29 18:03:13 +0700

    Create Лаб 3.cpp

---
+++ Лаб 3.cpp
@@ -0,0 +1,39 @@
+/*******************************
+ * Автор: Кондратенко Павел    *
+ * Вариант: 7                  *
+ *******************************/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  double d;
+  const double g = 9.8;
+  double phi, Fr;
+  const int n = 6;
+  int v, i;
+ 
+  cout << "d = ";
+  cin  >> d;
+  d = d / 100.0;
+ 
+  cout << "v = ";
+  cin  >> v;
+ 
+  cout << fixed << setprecision(3);
+ 
+  for (i = 0; i < n; ++i) {
+    cout << "v = ";
+    cin  >> v;
+    Fr = (v * v) / (g * d);
+    if (Fr <  50.0)
+      phi = 0.05 * pow(Fr, 0.2);
+    else
+      phi = 0.35 * pow(Fr, 0.1);
+   
+  cout << "phi =" << phi << "\t \t" << Fr << endl;
+}
+  return 0;
+}
