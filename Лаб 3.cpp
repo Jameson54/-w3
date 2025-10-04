7a04abc506bc066637b2866a32d74dd37f6e1c10
Jameson54 <hehlol123123@mail.ru>
2025-10-04 14:42:30 +0700

    Внёс изменения в лабу 3

--- Лаб 3.cpp
+++ Лаб 3.cpp
@@ -9,23 +9,22 @@
 
 using namespace std;
 
+const double g = 9.8;
+
 int main() {
   double d;
-  const double g = 9.8;
   double phi, Fr;
-  double n = 6.0;
-  double v, index;
+  int vCount = 6.0;
+  int v, index;
+  int countOfDigitsAfterTheDecimalPoint = 3.0;
+  
  
   cout << "d = ";
   cin  >> d;
-  d = d / 100.0;
  
-  cout << "v = ";
-  cin  >> v;
+  cout << fixed << setprecision(countOfDigitsAfterTheDecimalPoint);
  
-  cout << fixed << setprecision(3);
- 
-  for (index = 0; index < n; ++index) {
+  for (int vIndex = 0; vIndex < vCount; ++vIndex) {
     cout << "v = ";
     cin  >> v;
     
@@ -33,12 +32,12 @@ int main() {
     
     if (Fr <  50.0) {
       phi = 0.05 * pow(Fr, 0.2);
-    }
-    else {
+    } else {
       phi = 0.35 * pow(Fr, 0.1);
     }
     
   cout << "phi =" << phi << "\t \t" << Fr << endl;
   }
+  
   return 0;
 }
